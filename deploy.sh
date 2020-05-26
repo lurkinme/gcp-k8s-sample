68961d02f8ac35071dae16631fd255fa0f3544f2
Yury Smuraha <yury_smuraha@epam.com>
2020-05-26 22:41:25 +0300

    Update config again

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t smurahy/docker-fib-client:latest -t smurahy/docker-fib-client:$SHA -f ./client/Dockerfile ./client
+docker build -t smurahy/docker-fib-server:latest -t smurahy/docker-fib-server:$SHA -f ./server/Dockerfile ./server
+docker build -t smurahy/docker-fib-worker:latest -t smurahy/docker-fib-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push smurahy/docker-fib-client:latest
+docker push smurahy/docker-fib-server:latest
+docker push smurahy/docker-fib-worker:latest
+
+docker push smurahy/docker-fib-client:$SHA
+docker push smurahy/docker-fib-server:$SHA
+docker push smurahy/docker-fib-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=smurahy/docker-fib-server:$SHA
+kubectl set image deployment/client-deployment client=smurahy/docker-fib-client:$SHA
+kubectl set image deployment/worker-deployment worker=smurahy/docker-fib-worker:$SHA
