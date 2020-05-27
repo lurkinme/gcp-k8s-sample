a4aaf4b3e313f2de2725ad5ade218fa8a0ef8171
Yury Smuraha <yury_smuraha@epam.com>
2020-05-27 17:10:14 +0300

    Test Azure

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2 className="App-title">Fib calculator FOR KUBERNETES</h2>
+            <h2 className="App-title">Fib calculator FOR KUBERNETES in Azure</h2>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
